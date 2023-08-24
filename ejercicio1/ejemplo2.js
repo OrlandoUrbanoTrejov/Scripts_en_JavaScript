8a5355cf7b312a96a4b6fbff85e06aae505b2cb5
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-08-24 10:59:29 -0600

    Carpeta ejercicio1

---
+++ ejercicio1/ejemplo2.js
@@ -0,0 +1,9 @@
+function calcularMultiplicacion() {
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+
+    var resultado = numero1 * numero2;
+
+    document.getElementById("resultado").textContent = "El resultado es: " + resultado;
+}
+
